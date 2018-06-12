490659aa23a2d89392cf2155a15515564bbfbe68
Dave Strus <dave@getfretless.com>
2018-06-12 14:45:54 -0400

    Add a Message component that receives a prop

---
+++ src/MessageList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Message from './Message'
+
+const MessageList = () => {
+  const messages = [
+    {
+      id: 1,
+      userName: 'dstrus',
+      body: 'Chatting up a storm, yo!',
+    },
+    {
+      id: 2,
+      userName: 'dpalazzo',
+      body: 'This guy is so annoying. I hate my job.',
+    },
+  ]
+
+  return (
+    <div className="MessageList">
+      {
+        messages.map(msg => (
+          <Message key={msg.id} message={msg} />
+        ))
+      }
+    </div>
+  )
+}
+
+export default MessageList
