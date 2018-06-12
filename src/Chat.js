232ac96ccb59d029baa3dc47efa7c9ec17646a11
Dave Strus <dave@getfretless.com>
2018-06-12 15:14:24 -0400

    Add messages to Chat's state

--- src/Chat.js
+++ src/Chat.js
@@ -5,11 +5,30 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      messages: [
+        {
+          id: 1,
+          userName: 'stephen',
+          body: 'cool',
+        },
+        {
+          id: 2,
+          userName: 'dpalazzo',
+          body: 'This guy is so annoying. I hate my job.',
+        },
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="Chat">
         <ChatHeader />
-        <MessageList />
+        <MessageList messages={this.state.messages} />
         <MessageForm />
       </div>
     )
