c207b2258f4fba66dd0441f366be9a624bf47ab9
Dave Strus <dave@getfretless.com>
2018-06-13 09:48:51 -0400

    Add RoomList component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import UserInfo from './UserInfo'
+import RoomList from './RoomList'
 
 const Sidebar = (props) => {
   return (
@@ -9,24 +10,8 @@ const Sidebar = (props) => {
       style={styles.sidebar}
     >
       <UserInfo user={props.user} />
-      <h1
-        style={{
-          ...styles.children,
-          ...styles.h1,
-        }}
-      >
-        XTBC 18
-      </h1>
-      <nav
-        className="RoomList"
-        style={styles.children}
-      >
-        <h2>Rooms</h2>
-        <ul>
-          <li><a href="#">general</a></li>
-          <li><a href="#">random</a></li>
-        </ul>
-      </nav>
+      <h1 style={styles.h1}>XTBC 18</h1>
+      <RoomList />
     </aside>
   )
 }
@@ -41,14 +26,11 @@ const styles = {
     flexDirection: 'column',
   },
 
-  children: {
-    padding: '0 1rem',
-  },
-
   h1: {
     color: 'white',
     fontSize: '1.2rem',
     marginTop: 0,
+    padding: '0 1rem',
   },
 }
 
