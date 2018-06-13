0abca784692fc6688d79003d4690f6cb434a7755
Dave Strus <dave@getfretless.com>
2018-06-13 11:01:05 -0400

    Implement authentication workflow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
 
 import './App.css'
+import SignIn from './SignIn'
 import Main from './Main'
 
 class App extends Component {
   state = {
-    user: {
-      uid: '76547',
-      userName: 'maggie',
-      email: 'maggie@aol.com',
-    }
+    user: {},
+  }
+
+  handleAuth = (user) => {
+    this.setState({ user })
+  }
+
+  signedIn = () => {
+    return this.state.user.uid
+  }
+
+  signOut = () => {
+    this.setState({
+      user: {},
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <Main user={this.state.user} />
+        {
+          this.signedIn()
+            ? <Main user={this.state.user} signOut={this.signOut} />
+            : <SignIn handleAuth={this.handleAuth} />
+        }
       </div>
     )
   }
