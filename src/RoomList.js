70a9cc683e5fb1f6bc933a7e972ce1b5e726be0a
Dave Strus <dave@getfretless.com>
2018-06-14 10:55:51 -0400

    Show and hide new room form

--- src/RoomList.js
+++ src/RoomList.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
 
 import RoomLink from './RoomLink'
+import RoomForm from './RoomForm'
 import base from './base'
 
 class RoomList extends Component {
   state = {
-    rooms: {}
+    rooms: {},
+    showRoomForm: false,
   }
 
   componentDidMount() {
@@ -19,6 +21,14 @@ class RoomList extends Component {
     )
   }
 
+  showRoomForm = () => {
+    this.setState({ showRoomForm: true })
+  }
+
+  hideRoomForm = () => {
+    this.setState({ showRoomForm: false })
+  }
+
   addRoom = (room) => {
     const rooms = {...this.state.rooms}
     rooms[room.name] = room
@@ -26,24 +36,36 @@ class RoomList extends Component {
   }
 
   render() {
-    return (
-      <nav
-        className={`RoomList ${css(styles.nav)}`}
-      >
-        <h2 className={css(styles.h2)}>Rooms</h2>
-        <ul className={css(styles.list)}>
-          {
-            Object.keys(this.state.rooms).map(roomName => (
-              <RoomLink
-                key={roomName}
-                room={this.state.rooms[roomName]}
-                loadRoom={this.props.loadRoom}
-              />
-            ))
-          }
-        </ul>
-      </nav>
-    )
+    if (this.state.showRoomForm) {
+      return <RoomForm hideRoomForm={this.hideRoomForm} />
+    } else {
+      return (
+        <nav
+          className={`RoomList ${css(styles.nav)}`}
+        >
+          <div className={css(styles.heading)}>
+            <h2 className={css(styles.h2)}>Rooms</h2>
+            <button
+              className={css(styles.button)}
+              onClick={this.showRoomForm}
+            >
+              <i className="fas fa-plus-circle" title="Add room"></i>
+            </button>
+          </div>
+          <ul className={css(styles.list)}>
+            {
+              Object.keys(this.state.rooms).map(roomName => (
+                <RoomLink
+                  key={roomName}
+                  room={this.state.rooms[roomName]}
+                  loadRoom={this.props.loadRoom}
+                />
+              ))
+            }
+          </ul>
+        </nav>
+      )
+    }
   }
 }
 
@@ -60,6 +82,27 @@ const styles = StyleSheet.create({
     listStyle: 'none',
     marginLeft: 0,
     paddingLeft: 0,
+  },
+
+  heading: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  button: {
+    border: 0,
+    backgroundColor: 'transparent',
+    outline: 0,
+    padding: 0,
+    fontSize: '1rem',
+    color: 'rgba(255,255,255, 0.4)',
+    cursor: 'pointer',
+    transition: 'color 0.25s ease-out',
+
+    ':hover': {
+      color: 'white',
+    }
   },
 })
 
