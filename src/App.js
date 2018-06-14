44d925cd155cd4d94d0dac2fa82270488b9c344c
Dave Strus <dave@getfretless.com>
2018-06-14 11:49:10 -0400

    Save only a portion of the user object in state

--- src/App.js
+++ src/App.js
@@ -18,7 +18,12 @@ class App extends Component {
     }
   }
 
-  handleAuth = (user) => {
+  handleAuth = (oauthUser) => {
+    const user = {
+      email: oauthUser.email,
+      uid: oauthUser.uid,
+      displayName: oauthUser.displayName,
+    }
     this.setState({ user })
     localStorage.setItem('user', JSON.stringify(user))
   }
