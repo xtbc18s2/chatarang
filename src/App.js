6740522014a5d615477d4a564e154ad5196565ff
Dave Strus <dave@getfretless.com>
2018-06-18 16:07:39 -0400

    Update routes for auth flow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import './App.css'
 import { auth } from './base'
@@ -58,26 +58,34 @@ class App extends Component {
     return (
       <div className="App">
         <Switch>
-          <Route path="/sign-in" component={SignIn} />
+          <Route
+            path="/sign-in"
+            render={navProps => (
+              this.signedIn()
+                ? <Redirect to="/rooms/general" />
+                : <SignIn />
+            )}
+          />
           <Route
             path="/rooms/:roomName"
-            render={
-              navProps => (
-                <Main
-                  user={this.state.user}
-                  signOut={this.signOut}
-                  {...navProps}
-                />
-              )
-            }
+            render={navProps => (
+              this.signedIn()
+                ? <Main
+                    user={this.state.user}
+                    signOut={this.signOut}
+                    {...navProps}
+                  />
+                : <Redirect to="/sign-in" />
+            )}
+          />
+          <Route
+            render={navProps => (
+              this.signedIn()
+                ? <Redirect to="/rooms/general" />
+                : <Redirect to="/sign-in" />
+            )}
           />
         </Switch>
-
-        {/* {
-          this.signedIn()
-            ? <Main user={this.state.user} signOut={this.signOut} />
-            : <SignIn />
-        } */}
       </div>
     )
   }
