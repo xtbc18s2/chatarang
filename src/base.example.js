64c4246b221cca279f55b65e695cae9c7fc14d31
Dave Strus <dave@getfretless.com>
2018-06-20 15:31:24 -0400

    Add GitHub authentication

--- src/base.example.js
+++ src/base.example.js
@@ -16,6 +16,7 @@ const config = {
 const app = firebase.initializeApp(config)
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider()
+export const githubProvider = new firebase.auth.GithubAuthProvider()
 export const auth = firebase.auth()
 
 const db = app.database()
